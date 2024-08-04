bb491c7edfc6b766f61488069390c0f5f1d5d37d
kush parsaniya <kushparsaniya94@gmail.com>
2024-08-04 10:28:43 +0530

    security changes.

--- src/main/java/dev/kush/spotifyyoutubesyncbackend/config/security/WebAuthorizationConfig.java
+++ src/main/java/dev/kush/spotifyyoutubesyncbackend/config/security/WebAuthorizationConfig.java
@@ -1,5 +1,6 @@
 package dev.kush.spotifyyoutubesyncbackend.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,20 +9,27 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebAuthorizationConfig {
+
+    private final CustomAuthEntryPoint customAuthEntryPoint;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(
                         req -> req
+                                .requestMatchers("/cache/**").hasRole("ADMIN")
                                 .requestMatchers("/spotify/**", "/youtube/**",
                                         "sync/**", "/api/v1/sync/**", "/").permitAll()
                                 .requestMatchers("/WEB-INF/views/*.jsp").permitAll()
-                                .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(Customizer.withDefaults())
+                .exceptionHandling(e ->
+                        e.accessDeniedHandler((req, res, ex) -> res.setStatus(403))
+                )
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
